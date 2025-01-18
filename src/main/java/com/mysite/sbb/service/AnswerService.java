ce923a673b5191de809d089f715cdfbd8a522368
sem_in_github <semin6634@naver.com>
2025-01-18 14:51:52 +0900

    4일차: ch03-11 앵커 기능 추가하기 실습

--- src/main/java/com/mysite/sbb/service/AnswerService.java
+++ src/main/java/com/mysite/sbb/service/AnswerService.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content,
+    public Answer create(Question question, String content,
                        SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
@@ -24,6 +24,7 @@ public class AnswerService {
         answer.setCreatedDate(LocalDateTime.now());
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+        return answer;
     }
 
     public Answer getAnswer(Integer id) {
