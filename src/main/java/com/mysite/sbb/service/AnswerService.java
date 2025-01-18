187f2210afcab7e2121c56d3b46cae8c4bee3dd0
sem_in_github <semin6634@naver.com>
2025-01-18 14:07:54 +0900

    4일차: ch03-9 답변 수정, 삭제 기능 및 수정 일시 표시 실습

--- src/main/java/com/mysite/sbb/service/AnswerService.java
+++ src/main/java/com/mysite/sbb/service/AnswerService.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.domain.Answer;
 import com.mysite.sbb.domain.Question;
 import com.mysite.sbb.domain.SiteUser;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +23,26 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setCreatedDate(LocalDateTime.now());
         answer.setAuthor(author);
-        answerRepository.save(answer);
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()) {
+            return answer.get();
+        }
+        else{
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
